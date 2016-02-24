973a8443eb04fe0ec73b5aa5b4f7079cfc110272
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-24 16:13:32 +0100

    Option to put the scrit in wp_footer or wp_head

--- source/php/App.php
+++ source/php/App.php
@@ -38,11 +38,17 @@ class App
             'http://f1.eu.readspeaker.com/script/5507/ReadSpeaker.js?pids=embhl',
             array(),
             '1.0.0',
-            true
+            get_field('readspeaker-helper-script-footer', 'option')
         );
         wp_enqueue_script('readspeaker');
 
-        wp_register_script('readspeaker-helper', READSPEAKERHELPER_URL . '/dist/js/readspeaker-helper.min.js', array('readspeaker'), '1.0.0', true);
+        wp_register_script(
+            'readspeaker-helper',
+            READSPEAKERHELPER_URL . '/dist/js/readspeaker-helper.min.js',
+            array('readspeaker'),
+            '1.0.0',
+            get_field('readspeaker-helper-script-footer', 'option')
+        );
         wp_enqueue_script('readspeaker-helper');
     }
 }
